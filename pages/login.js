cf594501426799c1434e12480662e1c7cecf36ec
Pallabi Ghosh Das <pallabi@konsus.dk>
2022-08-15 14:47:43 +0200

    change it to className

--- pages/login.js
+++ pages/login.js
@@ -15,7 +15,7 @@ const LogIn = () => {
     return (
       <div>
         {/* <p>you are signed in</p> */}
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username"
+        <label className="block text-gray-700 text-sm font-bold mb-2" 
           onClick={() => signIn()}
           
         >
